c2c5ea75b432a85cca4215c5cae03bb6430cea6d
vikanshu <vikanshu2016@gmail.com>
2018-02-03 19:39:54 +0530

    Added fragments

--- app/src/main/java/com/vikanshu/echo/Activities/MainActivity.kt
+++ app/src/main/java/com/vikanshu/echo/Activities/MainActivity.kt
@@ -5,6 +5,10 @@ import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import com.vikanshu.echo.Fragments.AboutFragment
+import com.vikanshu.echo.Fragments.AllSongsFragment
+import com.vikanshu.echo.Fragments.FavouritesFragment
+import com.vikanshu.echo.Fragments.SettingsFragment
 import com.vikanshu.echo.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -19,6 +23,10 @@ class MainActivity : AppCompatActivity(){
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+        this.supportFragmentManager
+                .beginTransaction()
+                .add(R.id.frag_holder_main,AllSongsFragment())
+                .commit()
         all_songs.setBackgroundColor(resources.getColor(R.color.gray))
         favourites.setBackgroundColor(resources.getColor(R.color.white))
         settings.setBackgroundColor(resources.getColor(R.color.white))
@@ -30,6 +38,10 @@ class MainActivity : AppCompatActivity(){
         favourites.setBackgroundColor(resources.getColor(R.color.white))
         settings.setBackgroundColor(resources.getColor(R.color.white))
         about.setBackgroundColor(resources.getColor(R.color.white))
+        this.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frag_holder_main,AllSongsFragment())
+                .commit()
         drawer_layout.closeDrawers()
     }
     fun favourites(v: View){
@@ -37,24 +49,33 @@ class MainActivity : AppCompatActivity(){
         favourites.setBackgroundColor(resources.getColor(R.color.gray))
         settings.setBackgroundColor(resources.getColor(R.color.white))
         about.setBackgroundColor(resources.getColor(R.color.white))
+        this.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frag_holder_main,FavouritesFragment())
+                .commit()
         drawer_layout.closeDrawers()
-
     }
     fun settings(v: View){
         all_songs.setBackgroundColor(resources.getColor(R.color.white))
         favourites.setBackgroundColor(resources.getColor(R.color.white))
         settings.setBackgroundColor(resources.getColor(R.color.gray))
         about.setBackgroundColor(resources.getColor(R.color.white))
+        this.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frag_holder_main,SettingsFragment())
+                .commit()
         drawer_layout.closeDrawers()
-
     }
     fun about(v: View){
         all_songs.setBackgroundColor(resources.getColor(R.color.white))
         favourites.setBackgroundColor(resources.getColor(R.color.white))
         settings.setBackgroundColor(resources.getColor(R.color.white))
         about.setBackgroundColor(resources.getColor(R.color.gray))
+        this.supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frag_holder_main, AboutFragment())
+                .commit()
         drawer_layout.closeDrawers()
-
     }
 
     override fun onBackPressed() {
---
+++ app/src/main/java/com/vikanshu/echo/Fragments/AllSongsFragment.kt
@@ -0,0 +1,22 @@
+package com.vikanshu.echo.Fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.vikanshu.echo.R
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class AllSongsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_songs, container, false)
+    }
+}
