fac26dc856db02536d0f6d009339d684713cb854
vikanshu <36084899+vikanshu-joshi@users.noreply.github.com>
2018-02-10 20:55:50 +0530

    Update MainActivity.kt

--- app/src/main/java/com/vikanshu/echo/Activities/MainActivity.kt
+++ app/src/main/java/com/vikanshu/echo/Activities/MainActivity.kt
@@ -1,3 +1,24 @@
+/*
+    ECHO
+    Copyright (C) 2018  VIKANSHU
+    
+    This file is part of ECHO
+    
+    ECHO is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ECHO is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+    
+    contacting owner :- vikanshu2016@gmail.com
+*/
 package com.vikanshu.echo.Activities
 
 import android.media.AudioManager
